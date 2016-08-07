d36115fde27be366b7a3544ddb60b33816cd6631
Automaticism <prefetchisa@outlook.com>
2016-08-08 01:33:04 +0200

    Update PokeVisionFinder.py

--- PokeVisionFinder.py
+++ PokeVisionFinder.py
@@ -1,22 +1,18 @@
 import os
+from sys import argv
 import urllib2
 import traceback
 import argparse
-from time import time
+from time import sleep,time
 import json
 from pokemons import pokemonlist
 import httplib
 import re
 import requests
-import subprocess
-import wincolors
+import datetime
+import webbrowser
 
 __author__ = 'encode'
-__version__ = '0.1.5'
-
-_nt = False
-if os.name is "nt":
-    _nt = True
 
 _cities = []
 
@@ -27,9 +23,11 @@ _pokemonslisted = []
 _session =requests.Session()
 _sessionid = ""
 
-_scriptpath = os.path.abspath(os.path.dirname(__file__))
-ps_dir = os.path.join(_scriptpath,"Sniper")
-ps_path = os.path.join(ps_dir,"PokeSniper2.exe")
+_scriptpath = os.path.dirname(os.path.realpath(__file__))
+#PokeSniper2 Configuration
+ps_use = True
+ps_path = "C:\Users\prefe\Downloads\PokeSniper2_v1.10\PokeSniper2.exe"
+ps_dir = "C:\Users\prefe\Downloads\PokeSniper2_v1.10"
 
 #ErrorLogger
 def _logError(error):
@@ -40,8 +38,6 @@ def _logError(error):
 
 #JsonData
 def _jsondata(url):
-    if _nt and _colors:
-        wincolors.paint(wincolors.colors.ERROR)
     try:
         _rawdata = urllib2.urlopen(url)
         return json.load(_rawdata)
@@ -51,40 +47,38 @@ def _jsondata(url):
         elif _verbose == 2:
             print '[ERROR] HTTPError = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except urllib2.URLError, e:
         if _verbose == 1:
             print '[ERROR] URLError'
         elif _verbose == 2:
             print '[ERROR] URLError = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except httplib.HTTPException, e:
         if _verbose == 1:
             print '[ERROR] HTTPException'
         elif _verbose == 2:
             print '[ERROR] HTTPException = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except ValueError, e:
         if _verbose == 1:
             print '[ERROR] ValueError'
         elif _verbose == 2:
             print '[ERROR] ValueError = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except Exception:
         if _verbose == 1:
             print '[ERROR] generic exception: '
         elif _verbose == 2:
             print '[ERROR] generic exception: ' + traceback.format_exc()
         _logError(traceback.format_exc())
-        return url
+        return _jsondata(url)
 
 #JsonData Custom Headers
 def _jsondatach(url):
-    if _nt and _colors:
-        wincolors.paint(wincolors.colors.ERROR)
     try:
         _headers = { 'User-Agent' : 'Mozilla/5.0' }
         _req = urllib2.Request(url, None,_headers)
@@ -96,39 +90,37 @@ def _jsondatach(url):
         elif _verbose == 2:
             print '[ERROR] HTTPError = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except urllib2.URLError, e:
         if _verbose == 1:
             print '[ERROR] URLError'
         elif _verbose == 2:
             print '[ERROR] URLError = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except httplib.HTTPException, e:
         if _verbose == 1:
             print '[ERROR] HTTPException'
         elif _verbose == 2:
             print '[ERROR] HTTPException = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except ValueError, e:
         if _verbose == 1:
             print '[ERROR] ValueError'
         elif _verbose == 2:
             print '[ERROR] ValueError = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except Exception:
         if _verbose == 1:
             print '[ERROR] generic exception: '
         elif _verbose == 2:
             print '[ERROR] generic exception: ' + traceback.format_exc()
         _logError(traceback.format_exc())
-        return url
+        return _jsondata(url)
 
 def _jsondatachTrack(url):
-    if _nt and _colors:
-        wincolors.paint(wincolors.colors.ERROR)
     try:
         global sessionid
         if sessionid == "":
@@ -142,41 +134,39 @@ def _jsondatachTrack(url):
         elif _verbose == 2:
             print '[ERROR] HTTPError = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except urllib2.URLError, e:
         if _verbose == 1:
             print '[ERROR] URLError'
         elif _verbose == 2:
             print '[ERROR] URLError = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except httplib.HTTPException, e:
         if _verbose == 1:
             print '[ERROR] HTTPException'
         elif _verbose == 2:
             print '[ERROR] HTTPException = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except ValueError, e:
         if _verbose == 1:
             print '[ERROR] ValueError'
         elif _verbose == 2:
             print '[ERROR] ValueError = ' + str(e)
         _logError(str(e))
-        return url
+        return _jsondata(url)
     except Exception:
         if _verbose == 1:
             print '[ERROR] generic exception: '
         elif _verbose == 2:
             print '[ERROR] generic exception: ' + traceback.format_exc()
         _logError(traceback.format_exc())
-        return url
+        return _jsondata(url)
 
 #Find TrackMon Session
 def _findSessionIdTrack():
     global sessionid
-    if _nt and _colors:
-        wincolors.paint(wincolors.colors.INFO)
     print "[INFO] Finding SessionId for TrackMon"
     rw = re.compile('var sessionId \= \'(.*?)\'\;')
     r = _session.get("http://www.trackemon.com/")
@@ -185,8 +175,6 @@ def _findSessionIdTrack():
             suc = rw.search(line)
             if suc:
                 sessionid = suc.group(1)
-                if _nt and _colors:
-                    wincolors.paint(wincolors.colors.SUCCESS)
                 print "[INFO] SessionId for TrackMon Found"
             else:
                 _findSessionIdTrack()
@@ -202,26 +190,55 @@ def _pokesplit(pokemons):
 
 #POkePrinter
 def _printer(name,lat,lng,exp):
+    _time = time()
     _remain = float(exp)-time()
     _minutes = int(_remain / 60)
     _seconds = int(_remain % 60)
     _expire = str(_minutes) + " Minutes, " + str(_seconds) + " Seconds"
-    if _nt and _colors:
-        wincolors.paint(wincolors.colors.SUCCESS)
     print "-------------------------------------------------"
     print "Pokemon: " + name
     print "Coordinates: " + str(lat) + "," + str(lng)
     print "Expires in: " + _expire
+    print 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
     print "-------------------------------------------------"
     if _logging:
         _logPokemon(name, str(lat), str(lng), _expire)
+
+def _logCleanup():
+    os.chdir(_scriptpath)
+    with open("pokemons.log", "r") as f:
+        timestrings = f.readlines()
+        f.close()
+    with open("pokemons.log", "w") as f:
+        for timestring in timestrings:
+            expireindex = timestring.find("Expires in:")
+            expiretime = timestring[expireindex+len("Expires in: ["):timestring.find("[Timestamp")-2]
+            expireminindex = expiretime.find("Minutes")
+            expireminutes = expiretime[:expireminindex-1]
+            expiresecindex = expiretime.find("Seconds")
+            expireseconds = expiretime[expireminindex+len("Minutes, "):expiresecindex-1]
+            expireinseconds = (int(expireminutes) * 60) + int(expireseconds)
+            oldtimeindex = timestring.find("Timestamp: ")
+            oldtimeindex = oldtimeindex + len("Timestamp: ")
+            oldtime = timestring[oldtimeindex:]
+            oldtimeindex = oldtime.find("]")
+            oldtime = oldtime[:oldtimeindex]
+            oldtime = datetime.datetime.strptime(oldtime, '%Y-%m-%d %H:%M:%S')
+            xpiretime = oldtime + datetime.timedelta(seconds = expireinseconds)
+            nowtime = datetime.datetime.now()
+            timeleft = xpiretime - nowtime
+            expired = datetime.timedelta(seconds=45)
+            if timeleft >= expired:
+                f.write(timestring)
+        f.close()
 
 #Logger
 def _logPokemon(name, lat, lng, expire):
     os.chdir(_scriptpath)
     with open("pokemons.log", "a+") as f:
-        f.write("[" + name + "] [" + lat + "," + lng + "] [" + expire + "]\n")
+        f.write("[" + name + "] [" + lat + "," + lng + "] Expires in: [" + expire + "] [" +'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) +"]\n")
         f.close()
+    _logCleanup()
 
 #CoordsLoader
 def _populateCities():
@@ -235,8 +252,6 @@ def _populateCities():
 
 #Finder
 def _finderTrackemon(city):
-    if _nt and _colors:
-        wincolors.paint(wincolors.colors.INFO)
     print "[INFO] Looking pokemons in: " + city[0]
     _latitudesw = float(city[1]) - (0.05 * _zoomFactor)
     _longitudesw = float(city[2]) - (0.05 * _zoomFactor)
@@ -251,7 +266,7 @@ def _finderTrackemon(city):
             for pokemon in _scanurljsondata['pokemon']:
                 _id = pokemon['pokedexTypeId']
                 _name = _pokename(_id)
-                if _name.lower() in pokename.lower():
+                if pokename.lower() in _name.lower():
                     _lat = pokemon['latitude']
                     _lng = pokemon['longitude']
                     _exp = pokemon['expirationTime']
@@ -259,44 +274,34 @@ def _finderTrackemon(city):
                     if _id not in _pokemonslisted:
                         _pokemonslisted.append(_id)
                         _printer(_name, _lat, _lng, _exp)
-                        if ps_use: _pokeSniper(_name, str(_lat), str(_lng))
+                        if ps_use: 
+                            webbrowser.open_new("pokesniper2://"+name+"/"+str(lat)+","+str(lng))
+                            sleep(7)
                     else:
                         print "[INFO] Pokemon already listed found."
         except KeyError, e:
-            if _nt and _colors:
-                wincolors.paint(wincolors.colors.ERROR)
             if _verbose == 1:
                 print '[ERROR] KeyError'
             elif _verbose == 2:
                 import traceback
-                print '[ERROR] KeyError = ' + str(e)
+                print '[ERROR] KeyError= ' + str(e)
             _logError(str(e))
         except IndexError, e:
-            if _nt and _colors:
-                wincolors.paint(wincolors.colors.ERROR)
             if _verbose == 1:
                 print '[ERROR] IndexError'
             elif _verbose == 2:
-                print '[ERROR] IndexError = ' + str(e)
-            _logError(str(e))
-        except TypeError, e:
-            if _verbose == 1:
-                print '[ERROR] TypeError'
-            elif _verbose == 2:
-                print '[ERROR] TypeError= ' + str(e)
+                print '[ERROR] IndexError= ' + str(e)
             _logError(str(e))
 
 #Finder
 def _finderSkipLagged(city):
-    if _nt and _colors:
-        wincolors.paint(wincolors.colors.INFO)
     print "[INFO] Looking pokemons in: " + city[0]
     _latitudesw = float(city[1]) - (0.05 * _zoomFactor)
     _longitudesw = float(city[2]) - (0.05 * _zoomFactor)
     _latitudene = float(city[1]) + (0.05 * _zoomFactor)
     _longitudene = float(city[2]) + (0.05 * _zoomFactor)
 
-    _scanurl = "http://skiplagged.com/api/pokemon.php?bounds="+str(_latitudesw)+","+str(_longitudesw)+\
+    _scanurl = "https://skiplagged.com/api/pokemon.php?bounds="+str(_latitudesw)+","+str(_longitudesw)+\
                ","+str(_latitudene)+","+str(_longitudene)
     _scanurljsondata = _jsondatach(_scanurl)
 
@@ -305,7 +310,7 @@ def _finderSkipLagged(city):
             for pokemon in _scanurljsondata['pokemons']:
                 _id = pokemon['pokemon_id']
                 _name = _pokename(_id)
-                if _name.lower() in pokename.lower():
+                if pokename.lower() in _name.lower():
                     _lat = pokemon['latitude']
                     _lng = pokemon['longitude']
                     _exp = pokemon['expires']
@@ -313,58 +318,29 @@ def _finderSkipLagged(city):
                     if _combo not in _pokemonslisted:
                         _pokemonslisted.append(_combo)
                         _printer(_name, _lat, _lng, _exp)
-                        if ps_use: _pokeSniper(_name, str(_lat), str(_lng))
+                        if ps_use:  
+                            webbrowser.open_new("pokesniper2://"+_name+"/"+str(_lat)+","+str(_lng))
+                            sleep(7)
                     else:
                         print "[INFO] Pokemon already listed found."
         except KeyError, e:
-            if _nt and _colors:
-                wincolors.paint(wincolors.colors.ERROR)
             if _verbose == 1:
                 print '[ERROR] KeyError'
             elif _verbose == 2:
                 import traceback
-                print '[ERROR] KeyError = ' + str(e)
+                print '[ERROR] KeyError= ' + str(e)
             _logError(str(e))
         except IndexError, e:
-            if _nt and _colors:
-                wincolors.paint(wincolors.colors.ERROR)
             if _verbose == 1:
                 print '[ERROR] IndexError'
             elif _verbose == 2:
-                print '[ERROR] IndexError = ' + str(e)
-            _logError(str(e))
-        except TypeError, e:
-            if _verbose == 1:
-                print '[ERROR] TypeError'
-            elif _verbose == 2:
-                print '[ERROR] TypeError= ' + str(e)
+                print '[ERROR] IndexError= ' + str(e)
             _logError(str(e))
 
 #Sniper
 def _pokeSniper(name, lat, lng):
-    if _nt and _colors:
-        wincolors.paint(wincolors.colors.WARNING)
-    try:
-        if _terminal:
-            subp = subprocess.Popen([ps_path, name, lat, lng], cwd=ps_dir, creationflags = subprocess.CREATE_NEW_CONSOLE)
-            subp.wait()
-        else:
-            subp = subprocess.Popen([ps_path, name, lat, lng], cwd=ps_dir)
-            subp.wait()
-    except OSError, e:
-        error = "[WARNING] - PokeSniper2 Not found on Sniper folder"
-        if _nt and _colors:
-            wincolors.paint(wincolors.colors.ERROR)
-        if _verbose == 0:
-            print error
-        elif _verbose == 1:
-            print '[ERROR] OSError'
-            print error
-        elif _verbose == 2:
-            print '[ERROR] OSError = ' + str(e)
-            print error
-        _logError(str(e))
-        _logError(error)
+    os.chdir(ps_dir)
+    os.system(ps_path+" "+name+" "+lat+" "+lng)
 
 #Loop
 def _loop():
@@ -375,42 +351,24 @@ def _loop():
             _finderTrackemon(city)
 
 #Init
-_parser = argparse.ArgumentParser(description='PokeVisionFinder v'+__version__+' - encode')
-_parser.add_argument('-m','--mode', help='Mode of work', choices=["Skip", "Track","All"], default="All")
-_parser.add_argument('-s', '--sniper', help='No use sniper', action='store_false', default=True)
-_parser.add_argument('-S', '--sniperterminal', help='Sniper on a different terminal', action='store_true', default=False)
-_parser.add_argument('-l', '--loop', help='Run infinite', action='store_true', default=False)
-_parser.add_argument('-L','--logging', help='Log pokemons found', action='store_true', default=False)
-_parser.add_argument('-c','--catchfile', help='Use catch file', action='store_true', default=False)
-_parser.add_argument('-C','--colors', help='No use colors', action='store_false', default=True)
-_parser.add_argument('-p','--pokemons', help='List of pokemons', default="Pikachu")
-_parser.add_argument('-f','--factor', help='ZoomFactor', type=int, required=True, default=1)
-_parser.add_argument('-v','--verbose', help='Verbose mode', type=int, choices=[0, 1, 2], default=0)
-_args = _parser.parse_args()
+_useMode = "Skip"
 
-_useMode = _args.mode
+_logging = True
 
-ps_use = _args.sniper
+_zoomFactor = 1
 
-_terminal = True if ps_use and _args.sniperterminal else False
+_catchfile = True
 
-_logging = _args.logging
+_nonstop = True
 
-_zoomFactor = _args.factor
-
-_catchfile = _args.catchfile
-
-_colors = _args.colors
-
-_nonstop = _args.loop
-
-_verbose = _args.verbose
+_verbose = 0
 
 _inputpoke = ""
 
 if _catchfile:
     _inputpoke = [line.strip() for line in open("catch.txt", 'r')]
     _pokemons = _inputpoke
+    
 else:
     _inputpoke = _args.pokemons
     _pokesplit(_inputpoke)
@@ -418,15 +376,7 @@ else:
 if "Track" in _useMode or "All" in _useMode:
     _findSessionIdTrack()
 _populateCities()
-if _nt and _colors:
-    wincolors.paint(wincolors.colors.INFO)
-print """
-=====================================================================
-Welcome to PokeVisionFinder """ + __version__ + """
-Author: """ + __author__ + """
-Check out our Github at: https://github.com/encode32/PokeVisionFinder
-=====================================================================
-"""
+
 if _nonstop:
     while 1:
         _loop()
